a1e3ba853241687cda2533d598b33d01f7264a5b
Héctor Fernández <hector.fernandez@king.com>
2025-08-17 07:54:44 +0200

    2019 1

---
+++ puzzles/2019/01/src/lib.rs
@@ -0,0 +1,79 @@
+/// Calculates the fuel required for a given mass.
+///
+/// # Arguments
+/// * `mass` - The mass for which to calculate the fuel
+///
+/// # Returns
+/// The amount of fuel required for the given mass
+fn calculate_fuel(mass: u64) -> u64 {
+    (mass / 3).saturating_sub(2)
+}
+
+/// Parameters for solving Part 1 of the puzzle.
+pub struct Part1Parameters {
+    pub input_data: &'static str,
+}
+
+/// Solves Part 1 of the puzzle
+///
+/// # Arguments
+///
+/// * `params` - Parameters for the solver
+///
+/// # Returns
+///
+/// The solution as a string
+pub fn solve_part1(params: Part1Parameters) -> String {
+    let total_fuel: u64 = params
+        .input_data
+        .trim()
+        .lines()
+        .map(|line| line.trim().parse::<u64>().unwrap())
+        .map(|x| calculate_fuel(x))
+        .sum();
+    total_fuel.to_string()
+}
+
+/// Parameters for solving Part 2 of the puzzle.
+pub struct Part2Parameters {
+    pub input_data: &'static str,
+}
+
+/// Calculates the total fuel required, including the fuel for the fuel itself.
+///
+/// # Arguments
+/// * `mass` - The mass for which to calculate the total fuel
+///
+/// # Returns
+/// The total amount of fuel required for the given mass, including the fuel for the fuel
+fn calculate_total_fuel(mass: u64) -> u64 {
+    let mut total_fuel = 0;
+    let mut current_mass = mass;
+    while current_mass > 0 {
+        current_mass = calculate_fuel(current_mass);
+        if current_mass > 0 {
+            total_fuel += current_mass;
+        }
+    }
+    total_fuel
+}
+
+/// Solves Part 2 of the puzzle
+///
+/// # Arguments
+///
+/// * `params` - Parameters for the solver
+///
+/// # Returns
+///
+/// The solution as a string
+pub fn solve_part2(params: Part2Parameters) -> String {
+    let total_fuel: u64 = params
+        .input_data
+        .trim()
+        .lines()
+        .map(|line| line.trim().parse::<u64>().unwrap())
+        .map(|x| calculate_total_fuel(x))
+        .sum();
+    total_fuel.to_string()
+}
---
+++ puzzles/2019/01/tests/solution_tests.rs
@@ -0,0 +1,51 @@
+use aoc_2019_01::{Part1Parameters, Part2Parameters, solve_part1, solve_part2};
+use assertables::assert_not_empty;
+use include_dir::{Dir, include_dir};
+use pretty_assertions::assert_eq;
+use std::path::Path;
+
+static DATA_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/data");
+fn read_data_file<P: AsRef<Path>>(file_path: P) -> &'static str {
+    DATA_DIR
+        .get_file(file_path)
+        .unwrap()
+        .contents_utf8()
+        .unwrap()
+        .trim()
+}
+
+#[test]
+fn test_part1_real() {
+    let input_data = read_data_file("input/input.txt");
+    let result = solve_part1(Part1Parameters { input_data });
+    assert_not_empty!(&result);
+    let expected_result = read_data_file("solutions/solution_part1.txt");
+    assert_eq!(result, expected_result);
+}
+
+#[test]
+fn test_part1_test1() {
+    let input_data = read_data_file("input/input_part1_test1.txt");
+    let result = solve_part1(Part1Parameters { input_data });
+    assert_not_empty!(&result);
+    let expected_result = read_data_file("solutions/solution_part1_test1.txt");
+    assert_eq!(result, expected_result);
+}
+
+#[test]
+fn test_part2_real() {
+    let input_data = read_data_file("input/input.txt");
+    let result = solve_part2(Part2Parameters { input_data });
+    assert_not_empty!(&result);
+    let expected_result = read_data_file("solutions/solution_part2.txt");
+    assert_eq!(result, expected_result);
+}
+
+#[test]
+fn test_part2_test1() {
+    let input_data = read_data_file("input/input_part2_test1.txt");
+    let result = solve_part2(Part2Parameters { input_data });
+    assert_not_empty!(&result);
+    let expected_result = read_data_file("solutions/solution_part2_test1.txt");
+    assert_eq!(result, expected_result);
+}
