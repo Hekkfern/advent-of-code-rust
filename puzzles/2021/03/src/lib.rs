f9d4066b3a9914ea530d239231d6e3c4793e565b
Héctor Fernández <hector.fernandez@king.com>
2025-09-13 14:12:44 +0200

    2021 3

--- puzzles/2021/03/src/lib.rs
+++ puzzles/2021/03/src/lib.rs
@@ -66,29 +66,29 @@ enum Rating {
 
 fn filter(
     codes: &Vec<BitVec<u16, Msb0>>,
-    frequencies: &Vec<i32>,
     rating: Rating,
+    binary_length: usize,
 ) -> BitVec<u16, Msb0> {
     let mut indices: Interval<i32> = Interval::from_boundaries(0, codes.len() as i32 - 1);
     loop {
-        for i in 0..frequencies.len() {
-            let bit_criteria = match rating {
-                Rating::Oxygen => frequencies[i] >= 0,
-                Rating::CO2 => frequencies[i] < 0,
-            };
+        for i in 0..binary_length {
             let first_one_idx = codes
                 .iter()
                 .enumerate()
                 .filter(|(idx, _)| indices.contains(*idx as i32))
                 .find(|(_, code)| code[i])
                 .unwrap()
-                .0;
+                .0 as i32;
+            let bit_criteria = match rating {
+                Rating::Oxygen => first_one_idx <= indices.get_min() + (indices.count() as i32 / 2),
+                Rating::CO2 => !(first_one_idx <= indices.get_min() + (indices.count() as i32 / 2)),
+            };
             match bit_criteria {
                 true => {
-                    indices = Interval::from_boundaries(first_one_idx as i32, indices.get_max());
+                    indices = Interval::from_boundaries(first_one_idx, indices.get_max());
                 }
                 false => {
-                    indices = Interval::from_boundaries(indices.get_min(), first_one_idx as i32 - 1);
+                    indices = Interval::from_boundaries(indices.get_min(), first_one_idx - 1);
                 }
             }
             if indices.has_one_value() {
@@ -109,26 +109,27 @@ fn filter(
 /// The solution as a string
 pub fn solve_part2(params: Part2Parameters) -> String {
     // parse input data
-    let mut frequencies: Vec<i32> = vec![0; params.binary_length];
-    let mut codes: Vec<BitVec<u16, Msb0>> = Vec::new();
-    params.input_data.trim().lines().for_each(|line| {
-        let mut bv = bitvec![u16, Msb0; 0; params.binary_length];
-        line.trim().chars().enumerate().for_each(|(i, c)| {
-            if c == '1' {
-                frequencies[i] += 1;
-                bv.set(i, true);
-            } else {
-                frequencies[i] -= 1;
-            }
-        });
-        codes.push(bv);
-    });
+
+    let mut codes: Vec<_> = params
+        .input_data
+        .trim()
+        .lines()
+        .map(|line| {
+            let mut bv = bitvec![u16, Msb0; 0; params.binary_length];
+            line.trim().chars().enumerate().for_each(|(i, c)| {
+                if c == '1' {
+                    bv.set(i, true);
+                }
+            });
+            bv
+        })
+        .collect();
     // sort codes
     codes.sort_unstable();
     // determine ratings codes
-    let oxygen_code = filter(&codes, &frequencies, Rating::Oxygen);
-    let co2_code = filter(&codes, &frequencies, Rating::CO2);
+    let oxygen_code = filter(&codes, Rating::Oxygen, params.binary_length).load::<u32>();
+    let co2_code = filter(&codes, Rating::CO2, params.binary_length).load::<u32>();
     // calculate life support rating
-    let result = oxygen_code.load::<u32>() * co2_code.load::<u32>();
+    let result = oxygen_code * co2_code;
     result.to_string()
 }
